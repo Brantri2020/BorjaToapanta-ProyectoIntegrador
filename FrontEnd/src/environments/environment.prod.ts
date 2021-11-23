796b210a5095a3dd54f2e9bc7ea3c921e08c5944
Brandon <brandon.toapanta@epn.edu.ec>
2021-11-23 09:28:26 -0500

    firebase, creacion componentes login y registro

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCxci8i9oxirx_S0DhwNfpFLrvPXL61Dgc",
+  authDomain: "gad-parroquial-uyumbicho.firebaseapp.com",
+  projectId: "gad-parroquial-uyumbicho",
+  storageBucket: "gad-parroquial-uyumbicho.appspot.com",
+  messagingSenderId: "818910871207",
+  appId: "1:818910871207:web:1d5aa302fe5fea7d18e582"
+  }
 };
--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCxci8i9oxirx_S0DhwNfpFLrvPXL61Dgc",
+  authDomain: "gad-parroquial-uyumbicho.firebaseapp.com",
+  projectId: "gad-parroquial-uyumbicho",
+  storageBucket: "gad-parroquial-uyumbicho.appspot.com",
+  messagingSenderId: "818910871207",
+  appId: "1:818910871207:web:1d5aa302fe5fea7d18e582"
+  }
 };
 
 /*
