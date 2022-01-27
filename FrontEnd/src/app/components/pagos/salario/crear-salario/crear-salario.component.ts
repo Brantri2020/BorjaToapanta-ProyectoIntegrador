74aa8f23ad235a26f83b47d2da66620a02940d3e
GraceBorjaErazo <gracegbm@hotmail.es>
2022-01-27 18:42:12 -0500

    Salario CRUD Part 2

--- FrontEnd/src/app/components/pagos/salario/crear-salario/crear-salario.component.ts
+++ FrontEnd/src/app/components/pagos/salario/crear-salario/crear-salario.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Salario } from 'src/app/model/salario';
+import { MustMatch } from 'src/app/services/must-match.validator';
+import { SalarioService } from 'src/app/services/salario.service';
 
 @Component({
   selector: 'app-crear-salario',
@@ -7,9 +13,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearSalarioComponent implements OnInit {
 
-  constructor() { }
+  salarioForm: FormGroup;
+  titulo = 'Crear Salario';
+  id: string | null;
+  mensaje: string = "";
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService,
+    private _salarioService: SalarioService,
+    private aRouter: ActivatedRoute) {
+
+      this.salarioForm = this.fb.group({
+        cargo: ['', Validators.required],
+        salario: ['', Validators.required]
+      });
+      this.id = this.aRouter.snapshot.paramMap.get('id');
+
+     }
 
   ngOnInit(): void {
+    this.esEditar();
   }
+
+  agregarSalario() {
+    const SALARIO: Salario = {
+      cargo: this.salarioForm.get('cargo')?.value,
+      salario: this.salarioForm.get('salario')?.value
+    }
+    if (this.id !== null) {
+      //editamos salario
+      this._salarioService.editarSalario(this.id, SALARIO).subscribe(data => {
+        this.toastr.success('El Salario fue actualizado con éxito!', 'Salario Actualizado!');
+        this.router.navigate(['/salarios']);
+      }, error => {
+        console.log(error);
+        this.mensaje = error.error;
+      })
+    } else {
+      //agregamos salario
+      console.log(SALARIO);
+      this._salarioService.guardarSalario(SALARIO).subscribe(data => {
+        this.toastr.success('El salario fue registrado con éxito!', 'Salario Registrado!');
+        this.router.navigate(['/salarios']);
+      }, error => {
+        console.log(error);
+        this.mensaje = error.error;
+      })
+    }
+  }
+
+  esEditar() {
+    if (this.id !== null) {
+      this.titulo = 'Editar Salario';
+      this._salarioService.obtenerSalario(this.id).subscribe(data => {
+        this.salarioForm.setValue({
+
+          cargo: data.cargo,
+          salario: data.salario
+
+
+        })
+      })
+    }
+  }
+
+  
+
+  
+
 
 }
